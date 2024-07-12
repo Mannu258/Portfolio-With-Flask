2d9180663e914936c06f39fe3fdac91db8a69d2b
Your Name <you@example.com>
2024-07-12 10:25:14 +0530

    fix password error

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 # save this as app.py
-from flask import Flask,render_template,request ,send_from_directory
+from flask import Flask, render_template, request, send_from_directory
 from flask_mail import Mail, Message
 from threading import Thread
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
+
 app = Flask(__name__)
 mail = Mail(app)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'jeemannu90@gmail.com'
-app.config['MAIL_PASSWORD'] = 'cqep qxtj ijoy oaew '
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = "jeemannu90@gmail.com"
+app.config["MAIL_PASSWORD"] = "cqep qxtj ijoy oaew "
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 mail.init_app(app)
-
-
-
 
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
+
 
 def Submission_Mail(names, emails, subjects, msgs):
     # Email body for the confirmation to yourself
@@ -68,9 +67,11 @@ def Submission_Mail(names, emails, subjects, msgs):
 </html>
 """
     # Confirmation email to yourself
-    confirmation_msg = Message('New Portfolio Submission',
-                               sender='jeemannu90@gmail.com',
-                               recipients=['mandeepkumarmannu123@gmail.com', 'mishramandeep@outlook.com'])
+    confirmation_msg = Message(
+        "New Portfolio Submission",
+        sender="jeemannu90@gmail.com",
+        recipients=["mandeepkumarmannu123@gmail.com", "mishramandeep@outlook.com"],
+    )
     confirmation_msg.html = confirmation_body
     Thread(target=send_async_email, args=(app, confirmation_msg)).start()
 
@@ -121,13 +122,13 @@ def Submission_Mail(names, emails, subjects, msgs):
     </body>
 </html>
 """
-    thank_you_msg = Message('Thank You for Your Submission!',
-                            sender='jeemannu90@gmail.com',
-                            recipients=[emails])  # Corrected to use a list
+    thank_you_msg = Message(
+        "Thank You for Your Submission!",
+        sender="jeemannu90@gmail.com",
+        recipients=[emails],
+    )  # Corrected to use a list
     thank_you_msg.html = thank_you_body
     Thread(target=send_async_email, args=(app, thank_you_msg)).start()
-
-
 
 
 def Submission_Mail_MIS(names, emails, subjects, msgs):
@@ -175,9 +176,11 @@ def Submission_Mail_MIS(names, emails, subjects, msgs):
 </html>
 """
     # Confirmation email to yourself
-    confirmation_msg = Message(subjects,
-                               sender='jeemannu90@gmail.com',
-                               recipients=['mandeepkumarmannu123@gmail.com', 'mishramandeep@outlook.com'])
+    confirmation_msg = Message(
+        subjects,
+        sender="jeemannu90@gmail.com",
+        recipients=["mandeepkumarmannu123@gmail.com", "mishramandeep@outlook.com"],
+    )
     confirmation_msg.html = confirmation_body
     Thread(target=send_async_email, args=(app, confirmation_msg)).start()
 
@@ -228,118 +231,124 @@ def Submission_Mail_MIS(names, emails, subjects, msgs):
     </body>
 </html>
 """
-    thank_you_msg = Message('Thank You for Your Submission!',
-                            sender='jeemannu90@gmail.com',
-                            recipients=[emails])  # Corrected to use a list
+    thank_you_msg = Message(
+        "Thank You for Your Submission!",
+        sender="jeemannu90@gmail.com",
+        recipients=[emails],
+    )  # Corrected to use a list
     thank_you_msg.html = thank_you_body
     Thread(target=send_async_email, args=(app, thank_you_msg)).start()
-
-
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db = SQLAlchemy(app)
 
-@app.route('/robots.txt')
-@app.route('/sitemap.xml')
-@app.route('/BingSiteAuth.xml')
+
+@app.route("/robots.txt")
+@app.route("/sitemap.xml")
+@app.route("/BingSiteAuth.xml")
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
+
 class Details(db.Model):
-    ID = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(200),nullable=False)
-    email = db.Column(db.String(200),nullable=False)
-    subject = db.Column(db.String(200),nullable=False)
-    msg = db.Column(db.String(200),nullable=False)
+    ID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    subject = db.Column(db.String(200), nullable=False)
+    msg = db.Column(db.String(200), nullable=False)
 
     def __repr__(self) -> str:
         return f"{self.s_no} - {self.title}"
-    
+
+
 class Crediantials(db.Model):
-    ID = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(200),nullable=False)
-    password = db.Column(db.String(200),nullable=False)
+    ID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
     def __repr__(self) -> str:
         return f"{self.username}"
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     # Your custom logic here (logging, notifications, etc.)
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        names = request.form['names']
-        emails = request.form['emails']
-        subjects = request.form['subjects']
-        msgs = request.form['msgs']
-        Submission_Mail(names,emails,subjects,msgs)
-
-        
-
+        names = request.form["names"]
+        emails = request.form["emails"]
+        subjects = request.form["subjects"]
+        msgs = request.form["msgs"]
+        Submission_Mail(names, emails, subjects, msgs)
 
         details = Details(name=names, email=emails, subject=subjects, msg=msgs)
         db.session.add(details)
         db.session.commit()  # Commit the changes once
-        return render_template('thankyou.html')
-    return render_template('index.html')
+        return render_template("thankyou.html")
+    return render_template("index.html")
 
 
-@app.route("/project-showcase/<string:name>",methods=['POST','GET'])
+@app.route("/project-showcase/<string:name>", methods=["POST", "GET"])
 def projects(name):
     if name == "OG":
-        return render_template('Projects/OG.html')
+        return render_template("Projects/OG.html")
     elif name == "DB":
-        return render_template('Projects/DB.html')
+        return render_template("Projects/DB.html")
     elif name == "MAC":
-        return render_template('Projects/MAC.html')
+        return render_template("Projects/MAC.html")
     elif name == "CM":
-        return render_template('Projects/CM.html')
-    
+        return render_template("Projects/CM.html")
+
     return "404 Not Found"
 
 
-
-@app.route("/administrator",methods=['POST','GET'])
+@app.route("/administrator", methods=["POST", "GET"])
 def Admin():
     if request.method == "POST":
-        username = request.form['Username']
-        password = request.form['password']
-        admi = Crediantials.query.filter_by(username=username,password=password).first()
+        username = request.form["Username"]
+        password = request.form["password"]
+        admi = Crediantials.query.filter_by(
+            username=username, password=password
+        ).first()
         if admi:
             details = Details.query.all()
-            return render_template('Database.html',details=details)
+            return render_template("Database.html", details=details)
         else:
-            return render_template('login.html')
+            return render_template("403.html")
 
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@app.route("/profile-mis", methods=['POST', 'GET'])
+@app.route("/profile-mis", methods=["POST", "GET"])
 def MIS():
     if request.method == "POST":
-        names = request.form['names']
-        emails = request.form['emails']
-        subjects = "MIS Portfolio Submission: " + request.form.get('subjects', 'No Subject')
-        msgs = request.form['msgs']
-        Submission_Mail_MIS(names,emails,subjects,msgs)
+        names = request.form["names"]
+        emails = request.form["emails"]
+        subjects = "MIS Portfolio Submission: " + request.form.get(
+            "subjects", "No Subject"
+        )
+        msgs = request.form["msgs"]
+        Submission_Mail_MIS(names, emails, subjects, msgs)
         details = Details(name=names, email=emails, subject=subjects, msg=msgs)
         db.session.add(details)
         db.session.commit()  # Commit the changes once
-        return render_template('thankyou.html')
-    return render_template('MIS.html')
+        return render_template("thankyou.html")
+    return render_template("MIS.html")
 
-@app.route("/delete/<int:ID>", methods=['POST', 'GET'])
+
+@app.route("/delete/<int:ID>", methods=["POST", "GET"])
 def delete(ID):
     query = Details.query.get(ID)
     if query:
         db.session.delete(query)
         db.session.commit()
         return jsonify({"message": f"Item with ID {ID} deleted successfully"})
-   
 
-if __name__ == '__main__':
-   app.run(debug=True ,port=8000)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
